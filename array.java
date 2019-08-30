0c13c3924a6c0d7c5eba169739e490cb66b2da37
sakshidudhal712 <53616138+sakshidudhal712@users.noreply.github.com>
2019-08-30 10:28:13 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,11 @@
+class array
+{
+public static void main(String[] args)
+{
+int[] i={1,2,3,4,5};
+float[] f={1,2,3,4,5};
+char[] c={1,2,3,4,5};
+System.out.println("array length= "+i.length);
+System.out.println("array length= "+f.length);
+}
+}
